6dd053e31778f02f13bab70dd845976bed8b9514
Stakhan <elie84a.lt@gmail.com>
2021-02-25 20:34:08 +0100

    modified API to correctly receive 'init_point_picking' signal

--- src/extensions/mdInfinityAPI.js
+++ src/extensions/mdInfinityAPI.js
@@ -1,6 +1,8 @@
 
 class mdInfinityAPI{
-
+	/**
+	 * This class enables to manage a websocket connection to C++ software.
+	 */
 	constructor (viewer_obj, host='localhost', port='1111') {
 		this.viewer = viewer_obj
 		this.socket = new WebSocket('ws://'+host+':'+port)
@@ -11,6 +13,7 @@ class mdInfinityAPI{
 
 	initPointPicking () {
 		document.querySelector('[title="Point ID measurement"]').click()
+		console.debug('point picking initialized')
 	}
 	
 	sendId (e) {
@@ -18,34 +21,35 @@ class mdInfinityAPI{
 		if (typeof point["return number"] !== "undefined" && point["gps-time"] !== "undefined") {
 			console.log(point["return number"])
 			console.log(point["gps-time"])
-			var content = { return_number: point["return number"], gps_time: point["gps-time"] }
+			var content = { return_number: point["return number"][0], gps_time: point["gps-time"][0] }
 			this.socket.send(this.formatMessage("selected_point", content))
 			console.debug('ID sent')
 		}
 		else {
-			this.viewer.postMessage("Point Cloud doesn't fit requirement of having 'GpsTime' and 'return_number' attributes.")
+			//this.viewer.postMessage("Point Cloud doesn't fit requirement of having 'GpsTime' and 'return_number' attributes.")
 			console.debug('ID not sent')
 		}
 	}
 
 	onOpen (e) {		
-			console.debug('connection opened')
-			// Messages from Server
-			this.socket.onmessage = this.onMessage.bind(this)
+		console.debug('connection opened')
+		// Messages from Server
+		this.socket.onmessage = this.onMessage.bind(this)
 
-			// Messages to Server
-			this.viewer.measuringTool.addEventListener('point_selected', this.sendId.bind(this))
-			console.debug('--> C_API ready! B) <--')	
+		// Messages to Server
+		this.viewer.measuringTool.addEventListener('point_selected', this.sendId.bind(this))
+		console.debug('--> mdInfinity API ready! <--')	
 	}
 
 	onMessage (e) {
-				
-				if (e.data.type.includes('init_point_picking')){
-					this.initPointPicking()
-				}
-				if(e.data.type.includes('classified_point')){
-					this.onClassifiedPoint()
-				}
+		var type = JSON.parse(e.data).type
+		console.debug(type)
+		if (type.includes('init_point_picking')){ 
+			this.initPointPicking()
+		}
+		if(type.includes('classified_point')){
+			this.onClassifiedPoint()
+		}
 	}
 
 	formatMessage (type, content) {
